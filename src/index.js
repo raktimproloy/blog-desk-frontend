c5e657ddd48dfb36ddde1857457593ce87c392be
Raktim Proloy <83780881+raktimproloy@users.noreply.github.com>
2023-08-21 15:21:43 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import ContextApi from './ContextApi/ContextApi';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const API_Base = "https://blog-desk.onrender.com"
+const API_Base = "https://fine-tan-clam-wrap.cyclic.cloud"
+// const API_Base = "https://blog-desk.onrender.com"
 // const API_Base = "http://localhost:3001"
 root.render(
   <BrowserRouter>
